427975a77fbed8dea3a4ccbb96192565db4c904a
valentine <valentinetsinaga14@gmail.com>
2022-03-30 22:46:02 +0700

    add dialog fragment input name

--- app/src/main/java/com/valentine/allertdialog/AlertDialogFragment.kt
+++ app/src/main/java/com/valentine/allertdialog/AlertDialogFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 
 class AlertDialogFragment : DialogFragment() {
+    lateinit var name: String
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,9 +22,14 @@ class AlertDialogFragment : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //casting
+        val edtName = view.findViewById<EditText>(R.id.edtName)
+        val button = view.findViewById<Button>(R.id.fragment_dialog)
 
-        val button = view.findViewById<Button>(R.id.btnDialogFragment)
+       //getdata
         button.setOnClickListener {
+            name = edtName.text.toString().trim()
+            Toast.makeText(requireContext(), name, Toast.LENGTH_LONG).show()
             dialog?.dismiss()
         }
 
